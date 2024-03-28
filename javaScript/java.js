990bf46df2741acc43d22c7a8c73f325866ea6ef
Alexander <a.carlssons98@gmail.com>
2024-03-28 19:04:26 +0100

    Made playRound function
    Made a function called playRounc that takes playerSelection and computerSelection compares them to see who wins.

--- javaScript/java.js
+++ javaScript/java.js
@@ -11,8 +11,28 @@ console.log(computerSelection)
 let playerSelection = prompt("Choose Rock, Paper or Scissors!");
 
 playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
 
 function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return("Tie");
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return("You Lose!");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return("You Win!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return("You Win!")
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return("You Lose!")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return("You Lose!")
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return("You Win!")
+    }
 
 }
