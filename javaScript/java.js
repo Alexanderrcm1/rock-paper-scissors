7702482c637fa89759f4696597d84a6eb927e621
Alexander <a.carlssons98@gmail.com>
2024-03-28 20:36:54 +0100

    playGame Function, loop
    made a function called playGame. I put the playRound function inside the playGame function. Made a loop that plays the playRound function 5 times

--- javaScript/java.js
+++ javaScript/java.js
@@ -4,37 +4,39 @@ function getComputerChoice() {
 
 }
 
-let computerSelection = getComputerChoice()
-console.log(computerSelection)
+
+function playGame() {
+    for (let i = 1; i < 6; i++) {
+        let computerSelection = getComputerChoice()
+        console.log(computerSelection)
 
 
-let playerSelection = prompt("Choose Rock, Paper or Scissors!");
-
-playerSelection = playerSelection.toLowerCase();
-
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return("Tie");
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        return("You Lose! Paper beats rock");
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return("You Win! Rock beats scissors");
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return("You Win! Paper beats rock")
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return("You Lose! Scissors beats paper")
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return("You Lose! Rock beats scissors")
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return("You Win! Scissors beats paper")
+        let playerSelection = prompt("Choose Rock, Paper or Scissors!");
+        playerSelection = playerSelection.toLowerCase();
+        function playRound (playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                return("Tie");
+            }
+            else if (playerSelection === "rock" && computerSelection === "paper") {
+                return("You Lose! Paper beats rock");
+            }
+            else if (playerSelection === "rock" && computerSelection === "scissors") {
+                return("You Win! Rock beats scissors");
+            }
+            else if (playerSelection === "paper" && computerSelection === "rock") {
+                return("You Win! Paper beats rock")
+            }
+            else if (playerSelection === "paper" && computerSelection === "scissors") {
+                return("You Lose! Scissors beats paper")
+            }
+            else if (playerSelection === "scissors" && computerSelection === "rock") {
+                return("You Lose! Rock beats scissors")
+            }
+            else if (playerSelection === "scissors" && computerSelection === "paper") {
+                return("You Win! Scissors beats paper")
+            }
+        }
+        console.log(playRound(playerSelection,computerSelection));
     }
 }
-
-    
-console.log(playRound(playerSelection, computerSelection));
+console.log(playGame());
