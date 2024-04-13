ecfb1af52d2a76449efa24288b8242e9c1f3c454
Alexander <a.carlssons98@gmail.com>
2024-04-13 11:19:54 +0200

    Updated the javascript with buttons that chooses rock, paper or scissors instead of prompting

--- javaScript/java.js
+++ javaScript/java.js
@@ -4,61 +4,162 @@ function getComputerChoice() {
 
 }
 
-let player = 0;
-let computer = 0;
-
 function playGame() {
-    for (let i = 1; i < 6; i++) {
-        console.log("Round: " + i)
+    let player = 0;
+    let computer = 0;
+    let outcome;
 
+    const buttons = document.querySelector("#buttons");
+    const rockButton = document.querySelector("#rock");
+    const paperButton = document.querySelector("#paper");
+    const scissorsButton = document.querySelector("#scissors");
+
+    const container = document.querySelector("#container");
+    const playerStats = document.createElement("div");
+    const computerStats = document.createElement("div");
+    const gameStats = document.createElement("div");
+
+    playerStats.textContent = "Player Score: 0";
+    computerStats.textContent = "Computer Score: 0";
+
+    container.appendChild(playerStats);
+    container.appendChild(computerStats);
+    container.appendChild(gameStats);
+
+
+    rockButton.addEventListener("click", () => {
+        if (player === 5) {
+            gameStats.textContent = "Congratz!! You won the game!!"
+            return;
+        }
+        else if (computer === 5) {
+            gameStats.textContent = "Sorry....Computer won the game!!"
+            return;
+        }
+        else if (player === 5 && computer === 5) {
+            gameStats.textContent = "The game is a tie!! Play again?"
+            return;
+        }
         let computerSelection = getComputerChoice()
         console.log(computerSelection)
+        playerSelection = "rock";
+        playRound(playerSelection, computerSelection);
+        outcome = playRound(playerSelection,computerSelection);
 
-
-        let playerSelection = prompt("Choose Rock, Paper or Scissors!");
-        playerSelection = playerSelection.toLowerCase();
-
-        function playRound (playerSelection, computerSelection) {
-            if (playerSelection === computerSelection) {
-                return("Tie");
-            }
-            else if (playerSelection === "rock" && computerSelection === "paper") {
-                return("You Lose! Paper beats rock");
-            }
-            else if (playerSelection === "rock" && computerSelection === "scissors") {
-                return("You Win! Rock beats scissors");
-            }
-            else if (playerSelection === "paper" && computerSelection === "rock") {
-                return("You Win! Paper beats rock")
-            }
-            else if (playerSelection === "paper" && computerSelection === "scissors") {
-                return("You Lose! Scissors beats paper")
-            }
-            else if (playerSelection === "scissors" && computerSelection === "rock") {
-                return("You Lose! Rock beats scissors")
-            }
-            else if (playerSelection === "scissors" && computerSelection === "paper") {
-                return("You Win! Scissors beats paper")
-            }
-        }
-        let outcome = playRound(playerSelection,computerSelection);
-        console.log(outcome)
-
-        let win = outcome.includes("Win");
-        if (win == true) {
+        if (outcome == "Player Wins!") {
             player++
+            computerStats.textContent = `Computer Score: ${computer} | (${computerSelection})`;
+            playerStats.textContent= `Player Score: ${player} | (${playerSelection})`;
+            gameStats.textContent = "Player wins this round!"
         }
-        else if (win == false) {
+        else if (outcome == "Computer Wins!") {
             computer++
+            computerStats.textContent = `Computer Score: ${computer} | (${computerSelection})`;
+            playerStats.textContent= `Player Score: ${player} | (${playerSelection})`
+            gameStats.textContent = "Computer wins this round!"
         }
-        console.log("Player: " + player)
-        console.log("Computer: " + computer)
-    }
-    if (player > computer) {
-        console.log("Player Wins!")
-    }
-    else if (player < computer) {
-        console.log("Computer Wins!")
-    }
+        else {
+            player++
+            computer++
+            computerStats.textContent = `Computer Score: ${computer} | (${computerSelection})`;
+            playerStats.textContent= `Player Score: ${player} | (${playerSelection})`
+            gameStats.textContent = "It's a tie!"
+        }
+    });
+    
+    paperButton.addEventListener("click", () => {
+        if (player === 5) {
+            gameStats.textContent = "Congratz!! You won the game!!"
+            return;
+        }
+        else if (computer === 5) {
+            gameStats.textContent = "Sorry....Computer won the game!!"
+            return;
+        }
+        else if (player === 5 && computer === 5) {
+            gameStats.textContent = "The game is a tie!! Play again?"
+            return;
+        }
+        let computerSelection = getComputerChoice()
+        playerSelection = "paper";
+        console.log(playRound(playerSelection, computerSelection));
+        outcome = playRound(playerSelection,computerSelection);
+
+        if (outcome == "Player Wins!") {
+            player++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "Player wins this round!"
+        }
+        else if (outcome == "Computer Wins!") {
+            computer++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "Computer wins this round!"
+        }
+        else {
+            player++
+            computer++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "It's a tie!"
+        }
+    });
+    
+    scissorsButton.addEventListener("click", () => {
+        if (player === 5) {
+            gameStats.textContent = "Congratz!! You won the game!!"
+            return;
+        }
+        else if (computer === 5) {
+            gameStats.textContent = "Sorry....Computer won the game!!"
+            return;
+        }
+        else if (player === 5 && computer === 5) {
+            gameStats.textContent = "The game is a tie!! Play again?"
+            return;
+        }
+        let computerSelection = getComputerChoice()
+        playerSelection = "scissors";
+        playRound(playerSelection, computerSelection);
+        outcome = playRound(playerSelection,computerSelection);
+
+        if (outcome == "Player Wins!") {
+            player++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "Player wins this round!"
+        }
+        else if (outcome == "Computer Wins!") {
+            computer++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "Computer wins this round!"
+        }
+        else {
+            player++
+            computer++
+            computerStats.textContent = `Computer Score: ${computer}`;
+            playerStats.textContent= `Player Score: ${player}`;
+            gameStats.textContent = "It's a tie!"
+        }
+    });
+        
+    function playRound (playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return("Tie");
+        }
+        else if (
+                (playerSelection === "rock" && computerSelection === "scissors") || 
+                (playerSelection === "paper" && computerSelection === "rock") ||
+                (playerSelection === "scissors" && computerSelection === "paper")
+                ){
+            return("Player Wins!");
+        }
+        else {
+            return ("Computer Wins!");
+        }}
 }
-playGame()
+playGame();
+
+
